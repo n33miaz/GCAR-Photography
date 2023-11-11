50c9cefe55c87471a7e8330777036c345edd31af
n33miaz <ncormino@gmail.com>
2023-11-11 01:05:50 -0300

    primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+/*  './js./{js}',
+    './src/pages/*.{html}',
+    './src/pages/js./{js}',*/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
